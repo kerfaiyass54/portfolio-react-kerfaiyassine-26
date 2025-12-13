6e72b35a440cf4222b3245359b0d06ec9dc8235e
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-13 17:47:59 +0100

    Updated chatbot gemini api

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -9,6 +9,8 @@ export const Chat: React.FC = () => {
 
     const inputRef = useRef<HTMLInputElement>(null);
 
+    const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+
 
 
 
@@ -65,9 +67,10 @@ When the user asks about him, you MUST answer using the above data.`
 
 
         try {
-            const response = await fetch(import.meta.env.VITE_API_URL, {
+            const response = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent", {
                 method: "POST",
-                headers: { "Content-Type": "application/json" },
+                headers: { "Content-Type": "application/json",
+                    "x-goog-api-key": GEMINI_API_KEY, },
                 body: JSON.stringify({ contents: formattedHistory })
             });
 
